9d4287ea2f807816ba9e1039068c00451f9ec5ec
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-27 21:21:44 -0400

    Add route, add template for projects, edit index to display views.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
-var myPort = angular.module("myPort", []);
+//Created my module. Passed through the angular routing module
+var myPort = angular.module("myPort", ["ngRoute"]);
 
-myPort.directive("post", function(){
+//Routing for my app
+myPort.config(function($routeProvider){
+    $routeProvider.when("/",
+    {
+      controller: "portController",
+      templateUrl: "views/home.html"
+    })
+    .when("/about", {
+      controller: "portController",
+      templateUrl: "views/about.html"
+    })
+    .when("/contact", {
+      controller: "portController",
+      templateUrl: "views/contact.html"
+    })
+    .when("/portfolio", {
+      controller: "portController",
+      templateUrl: "views/portfolio.html"
+    })
+    .when("/404", {
+      controller: "portController",
+      templateUrl: "views/404.html"
+    })
+    .otherwise({ redirectTo: "/404" });
+});
+//Created my directive to post a repeating project template
+myPort.directive("project", function(){
   return {
       restrict: "AE",
       templateUrl: "templates/projects.html",
